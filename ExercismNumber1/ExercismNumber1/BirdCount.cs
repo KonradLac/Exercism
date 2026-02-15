a28bf99d9ad2a50a71fcce3162b9aa184ac25065
Konrad Lacek <konrad.lacek@outlook.com>
2026-02-15 19:24:26 +0100

    Exercism Number 1

---
+++ ExercismNumber1/ExercismNumber1/BirdCount.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercismNumber1
+{
+    internal class BirdCount
+    {
+        private int[] birdsPerDay;
+        public BirdCount(int[] birdsPerDay)
+        {
+            this.birdsPerDay = birdsPerDay;
+        }
+        public static int[] LastWeek()
+        {
+            return [0, 2, 5, 3, 7, 8, 4];
+        }
+        public int Today()
+        {
+            return birdsPerDay[^1];
+        }
+        public void IncrementTodaysCount()
+        {
+            birdsPerDay[^1]++;
+        }
+        public bool HasDayWithoutBirds()
+        {
+            return birdsPerDay.Contains(0);
+        }
+        public int CountForFirstDays(int numberOfDays)
+        {
+            int sum = default;
+            foreach (int day in birdsPerDay[0..numberOfDays])
+            {
+                sum += day;
+            }
+            return sum;
+        }
+        public int BusyDays()
+        {
+            int count = default;
+            foreach (int day in birdsPerDay)
+            {
+                if (day >= 5)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ ExercismNumber1/ExercismNumber1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ExercismNumber1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Exercism Number 1");
+            Console.ResetColor();
+        }
+    }
+}
