6a6a92f5b9a6a56bb8303b169bd2ed830a7b8a3d
Konrad Lacek <konrad.lacek@outlook.com>
2026-02-16 07:48:15 +0100

    Exercism Number 3

---
+++ ExercismNumber3/ExercismNumber3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ExercismNumber3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Exercism Number 3");
+            Console.ResetColor();
+        }
+    }
+}
