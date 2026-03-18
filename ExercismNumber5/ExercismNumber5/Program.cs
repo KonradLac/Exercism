6d414bea812fd19d79e8f4cafbabcc1596e6e302
KonradL <konrad.lacek@outlook.com>
2026-03-18 21:02:41 +0100

    Exercism Number 5

---
+++ ExercismNumber5/ExercismNumber5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace ExercismNumber5
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Exercism Number 4");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ ExercismNumber5/ExercismNumber5/SqueakyClean.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercismNumber5
+{
+    internal static class SqueakyClean
+    {
+        public static string Clean(string identifier)
+        {
+            System.Text.StringBuilder sb = new();
+            bool kebabCase = false;
+            foreach (var c in identifier)
+            {
+                sb.Append(c switch
+                {
+                    _ when c >= 945 && c <= 969 => default,
+                    _ when kebabCase => char.ToUpperInvariant(c),
+                    _ when char.IsWhiteSpace(c) => '_',
+                    _ when char.IsControl(c) => "CTRL",
+                    _ when char.IsLetter(c) => c,
+                    _ => default,
+                });
+                kebabCase = c.Equals('-');
+            }
+            return sb.ToString();
+        }
+    }
+}
