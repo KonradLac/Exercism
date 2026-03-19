650f22440dcd4c30af313d5219aaa29423ca7b04
KonradL <konrad.lacek@outlook.com>
2026-03-19 10:56:23 +0100

    Exercism Number 7

---
+++ ExercismNumber7/ExercismNumber7/Identity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercismNumber7
+{
+    public struct Identity
+    {
+        public string Email { get; set; }
+
+        public string EyeColor { get; set; }
+    }
+}
---
+++ ExercismNumber7/ExercismNumber7/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ExercismNumber7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Exercism Authentication System C#");
+            Console.ResetColor();
+        }
+    }
+}
